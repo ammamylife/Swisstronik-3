ed01ddd10cf9a0d81fff06378b42dc58f1104ced
Nithresh Nithi <bw474501@gmail.com>
2024-08-10 07:34:00 +0000

    Initial commit

--- contracts/ZunXBT.sol
+++ contracts/ZunXBT.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ZunXBT is ERC721, Ownable {
-    constructor(address initialOwner) ERC721("43ad37c37f9660ee45857bf5a1753fd0af994b3055029c075ba7c457d2255b82nithi", "78") Ownable(initialOwner) {}
+    constructor(address initialOwner) ERC721("swtr", "1") Ownable(initialOwner) {}
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
